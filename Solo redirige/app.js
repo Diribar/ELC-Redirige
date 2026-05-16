25075edd9d40b31b19e7fad5f290520bb1bbfa11
Diego Iribarren <76135160+Diribar@users.noreply.github.com>
2026-05-16 12:17:08 -0300

    Mejora de procesos

---
+++ Solo redirige/app.js
@@ -0,0 +1,18 @@
+"use strict"; // redirige
+console.clear();
+
+// Requires
+const express = require("express");
+const app = express();
+
+// Entornos
+const entProd = true;
+
+// Listener
+const puerto = entProd ? 4205 : 80;
+app.listen(puerto, () => console.log("evangelicemoslacultura.com - Redireccionando..."));
+
+// Redirige a 'peliculas.elc'
+const url = "https://peliculasconvalorescatolicos";
+const urlHost = url + (entProd ? ".com" : ":3001");
+app.use((req, res) => res.redirect(urlHost + req.originalUrl));
